240594cd559335cfb9e3b390321cb7c6c7d927c4
Andrew Muncey <andrewmuncey@Andrews-MacBook-Pro.local>
2015-06-09 10:03:02 +0100

    Initial Commit

---
+++ Vehicle.swift
@@ -0,0 +1,23 @@
+//
+//  Vehicle.swift
+//  ISPFail
+//
+//  Created by Andrew Muncey on 09/06/2015.
+//  Copyright (c) 2015 University of Chester. All rights reserved.
+//
+
+import Foundation
+
+protocol Vehicle {
+    
+    func startEngine()
+    func accellerate()
+    func brake()
+    func lightsOn()
+    func indicateLeft()
+    func indicateRight()
+    func turnOnRadio()
+    func turnOffRadio()
+    func playCD()
+    func ejectCD()
+}
